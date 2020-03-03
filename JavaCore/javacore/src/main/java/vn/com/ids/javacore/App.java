959bad098535339d268dd103f68525472d002b38
TuanAnh <anh.nguyen@idsolutions.com.vn>
2020-03-03 21:49:23 +0700

    add first class for project
    initial project

---
+++ JavaCore/javacore/src/main/java/vn/com/ids/javacore/App.java
@@ -0,0 +1,16 @@
+package vn.com.ids.javacore;
+
+import vn.com.ids.javacore.datetime.DateTime;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        DateTime dt = new DateTime();
+        dt.getFirtDateOfWeekInYear(11);
+    }
+}
