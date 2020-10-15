8073243d8333b182cb0a9e3788eb4cfd26c60008
anhnguyenids <anh.nguyen@idsolutions.com.vn>
2020-10-15 15:15:27 +0700

    problem 2

---
+++ JavaCore/javacore/src/main/java/vn/com/ids/javacore/algorithm/leetcode/first50/ListNode.java
@@ -0,0 +1,18 @@
+package vn.com.ids.javacore.algorithm.leetcode.first50;
+
+public class ListNode {
+	int val;
+	ListNode next;
+
+	ListNode() {
+	}
+
+	ListNode(int val) {
+		this.val = val;
+	}
+
+	ListNode(int val, ListNode next) {
+		this.val = val;
+		this.next = next;
+	}
+}
