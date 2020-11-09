c9fbde35da93aa634b85c04c550b4fe8a1140d27
anhnguyenids <anh.nguyen@idsolutions.com.vn>
2020-11-09 13:38:53 +0700

    init problem #15

--- JavaCore/javacore/src/main/java/vn/com/ids/javacore/algorithm/leetcode/under51/LeetCode15.java
+++ JavaCore/javacore/src/main/java/vn/com/ids/javacore/algorithm/leetcode/under51/LeetCode15.java
@@ -2,30 +2,55 @@ package vn.com.ids.javacore.algorithm.leetcode.under51;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class LeetCode15 {
     public static void main(String[] args) {
-        int[] nums = { -1, 0, 1, 2, -1, -4 };
-        int[] uniqueNums = makeUnique(nums);
-        Arrays.sort(uniqueNums);
-        List<List<Integer>> result = new ArrayList<List<Integer>>();
-        
+        int[] nums = { 0, 0, 0 };
+        Arrays.sort(nums);
+        List<List<Integer>> result = threeSum(nums);
+        for (int i = 0; i < result.size(); i++) {
+            result.get(i).forEach(System.out::print);
+            System.out.println();
+        }
     }
 
-    public static int[] makeUnique(int[] values) {
-        return Arrays.stream(values).distinct().toArray();
-    }
-    
-    public void getTripSet(int[] nums) {
-        for(int i = 0; i <= nums.length - 3; i++) {
-            findDoubleSet(i+1, nums);
+    public static List<List<Integer>> threeSum(int[] nums) {
+        Set<List<Integer>> set = new HashSet<>();
+        for (int i = 0; i <= nums.length - 3; i++) {
+            int[] tempArr = Arrays.copyOfRange(nums, i + 1, nums.length);
+            List<ArrayList<Integer>> twoSums = twoSum(tempArr, 0 - nums[i]);
+            if (!twoSums.isEmpty()) {
+                for (ArrayList<Integer> item : twoSums) {
+                    item.add(nums[i]);
+                    Collections.sort(item);
+                    set.add(item);
+                }
+            }
         }
+
+        return new ArrayList<>(set);
     }
-    
-    public void findDoubleSet (int i, int[] nums) {
-        for (int j = 0; j < nums.length; j++) {
-            
+
+    public static List<ArrayList<Integer>> twoSum(int[] nums, int target) {
+        Map<Integer, Integer> numMap = new HashMap<>();
+        List<ArrayList<Integer>> result = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (numMap.containsKey(complement)) {
+                ArrayList<Integer> item = new ArrayList<>();
+                item.add(numMap.get(complement));
+                item.add(nums[i]);
+                result.add(item);
+            } else {
+                numMap.put(nums[i], nums[i]);
+            }
         }
+        return result;
     }
 }
