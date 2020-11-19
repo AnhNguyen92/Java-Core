cf541833a73d087c943609e2d133b65c89af27fa
anhnguyenids <anh.nguyen@idsolutions.com.vn>
2020-11-19 11:01:28 +0700

    leetcode #1431

---
+++ JavaCore/javacore/src/main/java/vn/com/ids/javacore/algorithm/leetcode/KidsWiththeGreatestNumberofCandies.java
@@ -0,0 +1,40 @@
+package vn.com.ids.javacore.algorithm.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/*
+ * LeetCode 1431
+*/
+
+public class KidsWiththeGreatestNumberofCandies {
+private static final Logger logger = LoggerFactory.getLogger(KidsWiththeGreatestNumberofCandies.class);
+    
+    public static void main(String[] args) {
+        int[] candies = new int[] { 2,3,5,1,3 };
+        int extraCandies = 3;
+        List<Boolean> result = kidsWithCandies(candies, extraCandies);
+        logger.info("{}", result);
+    }
+
+    public static List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        List<Boolean> result = new ArrayList<>();
+        int max = -1;
+        for (int i = 0; i < candies.length; i++) {
+            if (candies[i] > max) {
+                max = candies[i];
+            }
+        }
+        for (int i = 0; i < candies.length; i++) {
+            if (candies[i]  + extraCandies >= max) {
+                result.add(Boolean.TRUE);
+            } else {
+                result.add(Boolean.FALSE);
+            }
+        }
+        return result; 
+    }
+}
