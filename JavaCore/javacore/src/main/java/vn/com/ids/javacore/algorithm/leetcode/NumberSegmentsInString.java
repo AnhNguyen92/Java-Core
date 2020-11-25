910e3f37720cf9636c0f1227dbf086fe42649d1d
anhnguyenids <anh.nguyen@idsolutions.com.vn>
2020-11-25 11:55:54 +0700

    leetcode problem

---
+++ JavaCore/javacore/src/main/java/vn/com/ids/javacore/algorithm/leetcode/NumberSegmentsInString.java
@@ -0,0 +1,14 @@
+package vn.com.ids.javacore.algorithm.leetcode;
+
+public class NumberSegmentsInString {
+    public static void main(String[] args) {
+        String s = ", , , ,        a, eaefa";
+        String str = s.trim();
+        if (str.equals("")) {
+            System.out.println(0);
+            return;
+        }
+        String[] arr = str.trim().split("\\s+");
+        System.out.println(arr.length);
+    }
+}
