3e9dea757b4e0b45f72cbb5d6a1b9a2ebabd4a6f
anhnguyenids <anh.nguyen@idsolutions.com.vn>
2021-01-21 11:40:44 +0700

    leetcode

---
+++ JavaCore/javacore/src/main/java/vn/com/ids/javacore/algorithm/leetcode/SubtreeOfAnotherTree.java
@@ -0,0 +1,24 @@
+package vn.com.ids.javacore.algorithm.leetcode;
+
+import vn.com.ids.javacore.algorithm.leetcode.model.TreeNode;
+
+public class SubtreeOfAnotherTree {
+    public static void main(String[] args) {
+        TreeNode s = new TreeNode(3, new TreeNode(4, new TreeNode(1), null), new TreeNode(5, new TreeNode(2), null));
+        TreeNode t = new TreeNode(3, new TreeNode(1), new TreeNode(2));
+        System.out.println(isSubtree(s, t));
+    }
+
+    public static boolean isSubtree(TreeNode s, TreeNode t) {
+        if (s == null && t == null)
+            return true;
+        else if (s == null || t == null)
+            return false;
+
+        if (s.val == t.val && isSubtree(s.left, t.left) && isSubtree(s.right, t.right)) {
+            return true;
+        }
+
+        return isSubtree(s.left, t) || isSubtree(s.right, t);
+    }
+}
