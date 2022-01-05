e1c0a6f9c4169b298cd21b1df642ed29bee211f8
Tuan Anh <50473804+AnhNguyen92@users.noreply.github.com>
2022-01-05 15:34:51 +0700

    refactor method

--- JavaCore/javacore/src/main/java/javacore/algorithm/interview/BoxArray2.java
+++ JavaCore/javacore/src/main/java/javacore/algorithm/interview/BoxArray2.java
@@ -3,7 +3,12 @@ package javacore.algorithm.interview;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class BoxArray2 {
+    private static Logger logger = LoggerFactory.getLogger(BoxArray2.class);
+
     public static void main(String[] args) {
         List<Integer> boxA = initListBoxA();
         List<Integer> boxB = initListBoxB();
@@ -15,16 +20,9 @@ public class BoxArray2 {
         } else if (boxA.isEmpty() && !boxC.isEmpty()) {
             moveBox(0, boxC.size() - 1, boxB, boxA, boxC);
         }
-        System.out.print("Box A: ");
-        boxA.forEach(item -> System.out.print(item + ", "));
-        System.out.print("\nBox B: ");
-        boxB.forEach(item -> System.out.print(item + ", "));
-        System.out.print("\nBox C: ");
-        boxC.forEach(item -> System.out.print(item + ", "));
-        System.out.println();
+        logBoxListValues(boxA, boxB, boxC);
 
         // Check and reorder
-        
         int i = findMaxPositioninBoxList(boxA);
         while (i < boxA.size()) {
             moveBox(0, i - 1, boxA, boxB, boxC);
@@ -34,13 +32,7 @@ public class BoxArray2 {
             }
             i = findMaxPositioninBoxList(boxA);
         }
-        System.out.print("Box A: ");
-        boxA.forEach(item -> System.out.print(item + ", "));
-        System.out.print("\nBox B: ");
-        boxB.forEach(item -> System.out.print(item + ", "));
-        System.out.print("\nBox C: ");
-        boxC.forEach(item -> System.out.print(item + ", "));
-        System.out.println();
+        logBoxListValues(boxA, boxB, boxC);
     }
 
     private static int findMaxPositioninBoxList(List<Integer> boxA) {
@@ -57,38 +49,39 @@ public class BoxArray2 {
         int maxA;
         int maxB;
         int maxC;
-        while ((!boxA.isEmpty() && !boxB.isEmpty()) || (!boxB.isEmpty() && !boxC.isEmpty()) || (!boxA.isEmpty() && !boxC.isEmpty())) {
+        while (notFinishMergeProcess(boxA, boxB, boxC)) {
             maxA = boxA.isEmpty() ? Integer.MAX_VALUE : boxA.get(0);
             maxB = boxB.isEmpty() ? Integer.MAX_VALUE : boxB.get(0);
             maxC = boxC.isEmpty() ? Integer.MAX_VALUE : boxC.get(0);
-            System.out.println("=========================");
-            System.out.print("Box A: ");
-            boxA.forEach(item -> System.out.print(item + ", "));
-            System.out.print("\nBox B: ");
-            boxB.forEach(item -> System.out.print(item + ", "));
-            System.out.print("\nBox C: ");
-            boxC.forEach(item -> System.out.print(item + ", "));
-            System.out.println("\n=========================");
-            if (maxA >= maxB && maxA >= maxC) {
-                if (maxB >= maxC) {
-                    moveItem(boxC, boxB, boxA);
-                } else {
-                    moveItem(boxB, boxC, boxA);
-                }
-            } else if (maxB >= maxA && maxB >= maxC) {
-                if (maxC >= maxA) {
-                    moveItem(boxA, boxC, boxB);
-                } else {
-                    moveItem(boxC, boxB, boxA);
-                }
-            } else if (maxC >= maxA && maxC >= maxB) {
-                if (maxA >= maxB) {
-                    moveItem(boxB, boxA, boxC);
-                } else {
-                    moveItem(boxA, boxB, boxC);
-                }
+            logBoxListValues(boxA, boxB, boxC);
+            
+            if (maxA >= maxB && maxB >= maxC) {
+                moveItem(boxC, boxB, boxA);
+            } else if (maxA >= maxB && maxC >= maxB) {
+                moveItem(boxB, boxC, boxA);
+            } else if (maxB >= maxA && maxC >= maxA) {
+                moveItem(boxA, boxC, boxB);
+            } else if (maxB >= maxA && maxA >= maxC) {
+                moveItem(boxC, boxA, boxB);
+            }
+            else if (maxC >= maxA && maxA >= maxB) {
+                moveItem(boxB, boxA, boxC);
+            } else if (maxC >= maxA && maxB >= maxA) {
+                moveItem(boxA, boxB, boxC);
             }
         }
+    }
+
+    private static boolean notFinishMergeProcess(List<Integer> boxA, List<Integer> boxB, List<Integer> boxC) {
+        return (!boxA.isEmpty() && !boxB.isEmpty()) || (!boxB.isEmpty() && !boxC.isEmpty()) || (!boxA.isEmpty() && !boxC.isEmpty());
+    }
+
+    private static void logBoxListValues(List<Integer> boxA, List<Integer> boxB, List<Integer> boxC) {
+        logger.info("=========================");
+        logger.info("Box A: {} ", boxA);
+        logger.info("Box B: {}", boxB);
+        logger.info("Box C: {} ", boxC);
+        logger.info("=========================");
     }
 
     private static void moveItem(List<Integer> source, List<Integer> target, List<Integer> intermediary) {
@@ -102,10 +95,10 @@ public class BoxArray2 {
             source.remove(0);
         } else {
             moveBox(from, to - 1, source, intermediary, target);
-            if (target.isEmpty()) {
-                target.add(source.get(0));
-            } else {
+            if (!target.isEmpty()) {
                 target.add(0, source.get(0));
+            } else {
+                target.add(source.get(0));
             }
             source.remove(0);
             moveBox(from, to - 1, intermediary, target, source);
