a715e58ab0749539f8cc13509f1554d0d3dede8d
Tuan Anh <50473804+AnhNguyen92@users.noreply.github.com>
2022-04-29 16:00:35 +0700

    LeetCode #48

---
+++ javacore/src/main/java/javacore/algorithm/leetcode/under51/RotateImage.java
@@ -0,0 +1,19 @@
+package javacore.algorithm.leetcode.under51;
+
+/*
+ * LeetCode 48
+ */
+public class RotateImage {
+	public void rotate(int[][] matrix) {
+        int[][] arr = new int[matrix.length][matrix[0].length];
+        int n = matrix.length - 1;
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= n; j++) {
+                arr[j][n - i] = matrix[i][j];
+            }
+        }
+        for (int i = 0; i <= n; i++) {
+        	System.arraycopy(arr[i], 0, matrix[i], 0, n);
+        }
+    }
+}
