d21729c754a223e1df9613bf0282b359c2d8f686
Tuan Anh <50473804+AnhNguyen92@users.noreply.github.com>
2022-05-03 01:36:07 +0700

    leet cde#

---
+++ javacore/src/main/java/javacore/algorithm/leetcode/under51/GroupAnagrams.java
@@ -0,0 +1,32 @@
+package javacore.algorithm.leetcode.under51;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/*
+ * LeetCode 49
+ */
+public class GroupAnagrams {
+	public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, Integer> map = new HashMap<>();
+        List<List<String>> ans = new ArrayList<>();
+        for (String s : strs) {
+            char[] arr = s.toCharArray();
+            Arrays.sort(arr);
+            String str = String.valueOf(arr);
+            if (map.containsKey(str)) {
+                int i = map.get(str).intValue();
+                ans.get(i).add(s);
+            } else {
+                List<String> item = new ArrayList<>();
+                item.add(s);
+                ans.add(item);
+                map.put(str, ans.size() - 1);
+            }
+        }
+        return ans;
+    }
+}
