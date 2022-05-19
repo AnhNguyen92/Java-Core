6093bc4586865a2d6917b20025342dd65ad02720
Tuan Anh <50473804+AnhNguyen92@users.noreply.github.com>
2022-05-19 18:05:47 +0700

    update leetcode problems solution

---
+++ javacore/src/main/java/javacore/algorithm/leetcode/under1500/PathCrossing.java
@@ -0,0 +1,32 @@
+package javacore.algorithm.leetcode.under1500;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ * Leetcode 1496
+ */
+public class PathCrossing {
+	public boolean isPathCrossing(String path) {
+        int x = 0;
+        int y = 0;
+        List<String> lst = new ArrayList<>();
+        lst.add("0-0");
+        for (char c : path.toCharArray()) {
+            if (c == 'N') {
+                x++;
+            } else if (c == 'E') {
+                y++;
+            } else if (c == 'S') {
+                x--;
+            } else if (c == 'W') {
+                y--;
+            }
+            if (lst.contains(x + "-" + y)) {
+                return true;
+            }
+            lst.add(x + "-" + y);
+        }
+        return false;
+    }
+}
---
+++ javacore/src/main/java/javacore/algorithm/leetcode/under2200/CountElementsWithStrictlySmallerAndGreaterElements.java
@@ -0,0 +1,19 @@
+package javacore.algorithm.leetcode.under2200;
+
+import java.util.Arrays;
+
+/*
+ * LeetCode 2148
+ */
+public class CountElementsWithStrictlySmallerAndGreaterElements {
+	public int countElements(int[] nums) {
+        Arrays.sort(nums);
+        int count = 0;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] > nums[0] && nums[i] < nums[nums.length - 1]) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
