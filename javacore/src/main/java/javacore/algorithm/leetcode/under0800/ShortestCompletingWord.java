bc87a48ae155bb08e250b5337e69d25b89982099
Tuan Anh <50473804+AnhNguyen92@users.noreply.github.com>
2022-05-25 10:58:56 +0700

    update leetcode problems solution

---
+++ javacore/src/main/java/javacore/algorithm/leetcode/under0800/ShortestCompletingWord.java
@@ -0,0 +1,44 @@
+package javacore.algorithm.leetcode.under0800;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/*
+ * Leetcode 748
+ */
+public class ShortestCompletingWord {
+	public String shortestCompletingWord(String licensePlate, String[] words) {
+		Arrays.sort(words, (a, b) -> a.length() - b.length());
+		Map<Character, Integer> map = countApperance(licensePlate);
+		for (String word : words) {
+			if (isCompleteStr(map, word)) {
+				return word;
+			}
+		}
+		return "";
+	}
+
+	private Map<Character, Integer> countApperance(String licensePlate) {
+		Map<Character, Integer> map = new HashMap<>();
+		for (char c : licensePlate.toLowerCase().toCharArray()) {
+			if (c >= 'a' && c <= 'z') {
+				map.put(c, map.getOrDefault(c, 0) + 1);
+			}
+		}
+		return map;
+	}
+
+	private boolean isCompleteStr(Map<Character, Integer> map, String s) {
+		Map<Character, Integer> sMap = countApperance(s);
+		for (Map.Entry<Character, Integer> entry : map.entrySet()) {
+			Character key = entry.getKey();
+			Integer val = entry.getValue();
+			if (sMap.get(key) == null || sMap.get(key) < val)
+				return false;
+		}
+		return true;
+	}
+	
+	
+}
---
+++ javacore/src/main/java/javacore/algorithm/leetcode/under1900/MinimumDistanceToTheTargetElement.java
@@ -0,0 +1,17 @@
+package javacore.algorithm.leetcode.under1900;
+
+/*
+ * LeetCode 1848
+ */
+public class MinimumDistanceToTheTargetElement {
+	public int getMinDistance(int[] nums, int target, int start) {
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == target) {
+                min = Math.min(min, Math.abs(i - start));
+            }
+        }
+        
+        return min;
+    }
+}
