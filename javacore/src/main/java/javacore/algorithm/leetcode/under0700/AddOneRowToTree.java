f3c000f59cf3f665fc9af9ac425a9952f4d10010
Tuan Anh <50473804+AnhNguyen92@users.noreply.github.com>
2022-06-02 14:41:00 +0700

    update leetcode problems solution

---
+++ javacore/src/main/java/javacore/algorithm/leetcode/under0700/AddOneRowToTree.java
@@ -0,0 +1,28 @@
+package javacore.algorithm.leetcode.under0700;
+
+import javacore.algorithm.leetcode.model.TreeNode;
+
+/*
+ * LeetCode 623
+ */
+public class AddOneRowToTree {
+	public TreeNode addOneRow(TreeNode root, int val, int depth) {
+        if (depth > 1) {
+            travel(root, val, 1, depth);
+            return root;
+        }
+        return new TreeNode(val, root, null);
+    }
+    
+    private void travel(TreeNode root, int val, int rootDepth, int depth) {
+        if (rootDepth < depth - 1) {
+            if (root.left != null)
+                travel(root.left, val, rootDepth + 1, depth);
+            if (root.right != null)
+                travel(root.right, val, rootDepth + 1, depth);
+        } else {
+            root.left = new TreeNode(val, root.left, null);
+            root.right = new TreeNode(val, null, root.right);
+        }
+    }
+}
---
+++ javacore/src/main/java/javacore/algorithm/leetcode/under1900/MinimizeMaximumPairSumInArray.java
@@ -0,0 +1,20 @@
+package javacore.algorithm.leetcode.under1900;
+
+import java.util.Arrays;
+
+/*
+ * LeetCode 1877
+ */
+public class MinimizeMaximumPairSumInArray {
+	public int minPairSum(int[] nums) {
+		Arrays.sort(nums);
+		int max = 0;
+		int n = nums.length;
+		for (int i = 0; i < n / 2; i++) {
+			if (nums[i] + nums[n - 1 - i] > max) {
+				max = nums[i] + nums[n - 1 - i];
+			}
+		}
+		return max;
+	}
+}
