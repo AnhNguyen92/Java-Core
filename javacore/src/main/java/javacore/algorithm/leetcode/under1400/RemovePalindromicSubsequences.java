9cc64a208f3996d9543392c701435fcdbf40d848
Tuan Anh <50473804+AnhNguyen92@users.noreply.github.com>
2022-06-08 18:27:12 +0700

    update leetcode problems solution

--- javacore/src/main/java/javacore/algorithm/leetcode/under1400/RemovePalindromicSubsequences.java
+++ javacore/src/main/java/javacore/algorithm/leetcode/under1400/RemovePalindromicSubsequences.java
@@ -5,30 +5,27 @@ package javacore.algorithm.leetcode.under1400;
  */
 public class RemovePalindromicSubsequences {
 	public int removePalindromeSub(String s) {
-		if (s.length() == 0)
-			return 0;
 		int i = 0;
 		int j = s.length() - 1;
 		int count = 0;
-		do {
-			boolean isValid = isPalindromic(s, i, j);
-			if (isValid) {
+		while (i <= j) {
+			if (isPalindromic(s, i, j)) {
 				i = j + 1;
 				j = s.length() - 1;
 				count++;
 			} else {
 				j--;
 			}
-		} while (i <= j);
+		}
 		return (count > 2 ? 2 : count);
 	}
 
 	private boolean isPalindromic(String str, int start, int end) {
-		String input = str.substring(start, end + 1);
+		while (start < end) {
+			if (str.charAt(start++) != str.charAt(end--))
+				return false;
+		}
 
-		StringBuilder sb = new StringBuilder(input);
-		sb.reverse();
-
-		return input.equals(sb.toString());
+		return true;
 	}
 }
