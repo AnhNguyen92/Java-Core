3bb16fb76ac12d62cf56828b5cc6831c2740d65b
Tuan Anh <50473804+AnhNguyen92@users.noreply.github.com>
2022-06-28 17:15:30 +0700

    update leetcode problems solution

--- javacore/src/main/java/javacore/algorithm/leetcode/under0051/CountAndSay.java
+++ javacore/src/main/java/javacore/algorithm/leetcode/under0051/CountAndSay.java
@@ -6,26 +6,26 @@ package javacore.algorithm.leetcode.under0051;
 
 public class CountAndSay {
 
-    public String countAndSay(int n) {
-        if (n == 1) {
-            return "1";
-        }
-        StringBuilder builder = new StringBuilder("");
-        String previous = countAndSay(n - 1);
-        int i = 0;
-        int j = 0;
-        while (i < previous.length()) {
-            j = i;
-            while (j < previous.length() && previous.charAt(i) == previous.charAt(j)) {
-                j++;
-            }
+	public String countAndSay(int n) {
+		if (n == 1) {
+			return "1";
+		}
+		StringBuilder builder = new StringBuilder("");
+		String previous = countAndSay(n - 1);
+		int i = 0;
+		int j = 0;
+		while (i < previous.length()) {
+			j = i;
+			while (j < previous.length() && previous.charAt(i) == previous.charAt(j)) {
+				j++;
+			}
 
-            builder.append(j - i);
-            builder.append(previous.charAt(i));
-            i = j;
-        }
+			builder.append(j - i);
+			builder.append(previous.charAt(i));
+			i = j;
+		}
 
-        return builder.toString();
-    }
+		return builder.toString();
+	}
 
 }
---
+++ javacore/src/main/java/javacore/algorithm/leetcode/under1400/ReduceArraySizeToTheHalf.java
@@ -0,0 +1,26 @@
+package javacore.algorithm.leetcode.under1400;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+
+/*
+ * LeetCode 1338
+ */
+public class ReduceArraySizeToTheHalf {
+	public int minSetSize(int[] arr) {
+		Map<Integer, Integer> map = new HashMap<>();
+		PriorityQueue<Integer> queue = new PriorityQueue<>((a, b) -> b - a);
+		for (int i : arr) {
+			map.put(i, map.getOrDefault(i, 0) + 1);
+		}
+		map.values().forEach(queue::add);
+		int count = 0;
+		int total = 0;
+		while (total < arr.length / 2) {
+			total += queue.poll();
+			count++;
+		}
+		return count;
+	}
+}
