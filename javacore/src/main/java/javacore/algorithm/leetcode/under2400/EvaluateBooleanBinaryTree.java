fb093d08060f9ed44b9fe6d44ba9af4f034e1713
Tuan Anh <50473804+AnhNguyen92@users.noreply.github.com>
2022-07-12 16:45:59 +0700

    update leetcode problems solution

---
+++ javacore/src/main/java/javacore/algorithm/leetcode/under2400/EvaluateBooleanBinaryTree.java
@@ -0,0 +1,21 @@
+package javacore.algorithm.leetcode.under2400;
+
+import javacore.algorithm.leetcode.model.TreeNode;
+
+/*
+ * LeetCode 2331
+ */
+public class EvaluateBooleanBinaryTree {
+	public boolean evaluateTree(TreeNode root) {
+        if (root == null || root.val == 1) {
+            return true;
+        }
+        if (root.val == 0) {
+            return false;
+        }
+        if (root.val == 2) {
+            return evaluateTree(root.left) || evaluateTree(root.right);
+        }
+        return evaluateTree(root.left) && evaluateTree(root.right);
+    }
+}
