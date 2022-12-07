60dcde7aa6bafa6a4c333f1747e6f57ff918d386
Tuan Anh <50473804+AnhNguyen92@users.noreply.github.com>
2022-12-07 15:08:26 +0700

    upload leetcode solution of problem 565

---
+++ JavaCore/src/main/java/javacore/algorithm/leetcode/under0600/ArrayNesting.java
@@ -0,0 +1,28 @@
+package javacore.algorithm.leetcode.under0600;
+
+import java.util.Calendar;
+
+/*
+ * LeetCode 565. Array Partition
+ */
+public class ArrayNesting {
+
+    public int arrayNesting(int[] nums) {
+        boolean[] visited = new boolean[nums.length];
+        int res = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (!visited[i]) {
+                int start = nums[i], count = 0;
+                do {
+                    start = nums[start];
+                    count++;
+                    visited[start] = true;
+                }
+                while (start != nums[i]);
+                res = Math.max(res, count);
+            }
+        }
+        return res;
+    }
+
+}
