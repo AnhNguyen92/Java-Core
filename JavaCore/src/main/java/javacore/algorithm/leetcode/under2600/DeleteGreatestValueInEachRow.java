2cc0bf232c760c8ae1b0436d8f05cdfd55f60efe
Tuan Anh <50473804+AnhNguyen92@users.noreply.github.com>
2022-12-12 16:02:45 +0700

    update leetcode problem solution

---
+++ JavaCore/src/main/java/javacore/algorithm/leetcode/under2600/DeleteGreatestValueInEachRow.java
@@ -0,0 +1,27 @@
+package javacore.algorithm.leetcode.under2600;
+
+import java.util.Arrays;
+
+/*
+ * Leetcode 2500. Delete Greatest Value in Each Row
+ */
+public class DeleteGreatestValueInEachRow {
+	public int deleteGreatestValue(int[][] grid) {
+        int ans = 0;
+        for (int i = 0; i < grid.length; i++) {
+        	Arrays.sort(grid[i]);
+        }
+        int max;
+        for (int j = grid[0].length - 1; j >= 0; j--) {
+        	max = grid[grid.length -1][j];
+        	for (int i = 0; i < grid.length; i++) {
+            	if (grid[i][j] > max) {
+            		max = grid[i][j];
+            	}
+            }
+        	ans += max;
+        }
+        
+        return ans;
+    }
+}
