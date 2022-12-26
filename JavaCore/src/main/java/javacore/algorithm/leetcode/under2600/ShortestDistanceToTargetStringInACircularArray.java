935e34e60491a35933a65127e6d77ed9e29ad063
Tuan Anh <50473804+AnhNguyen92@users.noreply.github.com>
2022-12-26 17:45:04 +0700

    Create ShortestDistanceToTargetStringInACircularArray.java

---
+++ JavaCore/src/main/java/javacore/algorithm/leetcode/under2600/ShortestDistanceToTargetStringInACircularArray.java
@@ -0,0 +1,10 @@
+package javacore.algorithm.leetcode.under2600;
+
+/*
+ * LeetCode 2515. Shortest Distance to Target String in a Circular Array
+ */
+public class ShortestDistanceToTargetStringInACircularArray {
+//    public int closetTarget(String[] words, String target, int startIndex) {
+//        
+//    }
+}
