b32007e0ab4106d1d91078ef8ec5201c38e07be1
Tuan Anh <50473804+AnhNguyen92@users.noreply.github.com>
2023-01-30 09:23:21 +0700

    Create CountDistinctNumbersOnBoard.java

---
+++ JavaCore/src/main/java/javacore/algorithm/leetcode/under2600/CountDistinctNumbersOnBoard.java
@@ -0,0 +1,13 @@
+package javacore.algorithm.leetcode.under2600;
+
+/*
+ * LeetCode 2549. Count Distinct Numbers on Board
+ */
+public class CountDistinctNumbersOnBoard {
+    public int distinctIntegers(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        return n -1;
+    }
+}
