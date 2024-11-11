b986517040dc0849ef54728eb619e7c5a5e535f0
Tuan Anh <nguyentuananh30392@gmail.com>
2024-11-11 16:48:05 +0700

    update leetcode solution under 1800

---
+++ src/main/java/javacore/algorithm/leetcode/under1800/CheckIfNumberIsASumOfPowersOfThree.java
@@ -0,0 +1,26 @@
+package javacore.algorithm.leetcode.under1800;
+
+/**
+ * 1780. Check if Number is a Sum of Powers of Three
+ */
+public class CheckIfNumberIsASumOfPowersOfThree {
+    public boolean checkPowersOfThree(int n) {
+        if (n == 2) {
+            return false;
+        }
+        if (n == 1 || n == 3) {
+            return true;
+        }
+        int x = 3;
+        while (x * 3 <= n) {
+            x *= 3;
+        }
+        if (x < n) {
+            if (n - x >= x) {
+                return false;
+            }
+            return checkPowersOfThree(n - x);
+        }
+        return true;
+    }
+}
