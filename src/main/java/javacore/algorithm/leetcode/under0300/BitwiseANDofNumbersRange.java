115710e422e9f143f0af0a05d5bb1dc2226db90e
Tuan Anh <nguyentuananh30392@gmail.com>
2024-12-05 17:02:58 +0700

    merge conflict

--- src/main/java/javacore/algorithm/leetcode/under0300/BitwiseANDofNumbersRange.java
+++ src/main/java/javacore/algorithm/leetcode/under0300/BitwiseANDofNumbersRange.java
@@ -1,16 +1,16 @@
 package javacore.algorithm.leetcode.under0300;
 
-/*
- * LeetCode 201. Bitwise AND of Numbers Range
+/**
+ * 201. Bitwise AND of Numbers Range
  */
-public class BitwiseANDofNumbersRange {
-	public int rangeBitwiseAnd(int left, int right) {
-		int s = 0;
-		while (left != right) {
-			left = left >> 1;
-			right = right >> 1;
-			s++;
-		}
-		return (left << s);
-	}
+public class BitwiseANDOfNumbersRange {
+    public int rangeBitwiseAnd(int left, int right) {
+        int s = 0;
+        while(left != right){
+            left =  left >> 1;
+            right = right >> 1;
+            s++;
+        }
+        return (left << s);
+    }
 }
